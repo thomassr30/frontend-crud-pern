8de74565ebd64c6a9abc653638bcd0720a7beeeb
thomassr30 <thomassoto94@gmail.com>
2022-04-18 01:13:33 -0400

    Estructura navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import {
+  Box,
+  AppBar,
+  Container,
+  Toolbar,
+  Typography,
+  Button
+} from '@mui/material'
+import {Link, useNavigate} from 'react-router-dom'
 
 const Navbar = () => {
+
+  const navigate = useNavigate()
+
   return (
-    <div>Navbar</div>
+    <Box sx={{flexGrow: 1}}>
+      <AppBar position='static' color='transparent'>
+        <Container>
+          <Toolbar>
+            <Typography variant='h6' sx={{flexGrow: 1}}>
+              <Link to="/" style={{textDecoration: 'none', color: '#eee'}}>
+                PERN Stack
+              </Link>
+            </Typography>
+            <Button 
+                variant='contained' 
+                color='primary'
+                onClick={() => navigate('/tasks/new')}
+                >
+              New Task
+            </Button>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </Box>
   )
 }
 
